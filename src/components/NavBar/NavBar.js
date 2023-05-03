8061c3a900c66533b15880ee8aaf247c280e661c
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-05-03 13:02:43 +0200

    Add NavLink component with isActive class

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,5 +1,6 @@
 import styles from './Navbar.module.scss';
 import Container from '../Container/Container';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -9,15 +10,9 @@ const NavBar = () => {
           <i className={'fa fa-tasks'}></i>
         </a>
         <ul className={styles.navlinks}>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/favorite">Favorite</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
+          <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+          <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+          <li><NavLink className={({ isActive }) => isActive ? styles.linkActive : undefined} to="/about">About</NavLink></li>
         </ul>
       </Container>
     </nav>
